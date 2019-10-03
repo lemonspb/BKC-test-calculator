0e16f5b268f2fc5b31d8b8f662e259b96c336592
Misha Skopenko <misha.skopenko@yandex.ru>
2019-10-04 06:32:50 +0700

    refactor

--- src/Components/Range/range.js
+++ src/Components/Range/range.js
@@ -1,49 +1,57 @@
-import React, { useState } from 'react'
-import './range.scss'
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-const  Range = ({valueChange})=> {
+import React, { useState } from "react";
+import "./range.scss";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+const Range = ({ valueChange }) => {
+  const [duration, setDuration] = useState(50);
 
-const [duration, setDuration ] =  useState(50)
+  valueChange(duration);
 
-valueChange(duration)
+  const marks = {
+    50: <strong>50&ensp;т.</strong>,
+    500: <strong>500 &ensp;т.</strong>,
+    1000: <strong>1000&ensp;т.</strong>,
+    2000: <strong>2000&ensp;т.</strong>,
+    3000: <strong>3000&ensp;т.</strong>
+  };
 
-const marks = {
-  50: <strong>50 т.</strong>,
-  500: <strong>500 т.</strong>,
-  1000: <strong>1000 т.</strong>,
-  2000: <strong>2000 т.</strong>,
-  3000: <strong>3000 т.</strong>
+  const handleChangeValue = e => setDuration(e);
+
+  return (
+    <div className="range">
+      <div className="range__sum">
+        <span className="range__text">Сумма для инвестирования</span>{" "}
+        <div className="range__money">
+          {Number(duration * 1000).toLocaleString("ru-RU")}&ensp;&#8381;
+        </div>
+      </div>
+
+      <form className="range__form">
+        <Slider
+          min={50}
+          max={3000}
+          marks={marks}
+          onChange={handleChangeValue}
+          step={1}
+          defaultValue={duration}
+          railStyle={{ background: "grey", height: "5px" }}
+          trackStyle={{ height: "5px", background: "#4C9AFF" }}
+          handleStyle={{
+            width: "25px",
+            height: "25px",
+            border: "#4C9AFF",
+            marginTop: "-10px"
+          }}
+          dotStyle={{
+            width: "15px",
+            height: "15px",
+            marginBottom: "-3px",
+            border: "#4C9AFF"
+          }}
+        />
+      </form>
+    </div>
+  );
 };
 
- const  handleChangeValue = (e) => setDuration(e)
-
-    return (
-
-    <div className='range'>
-         <div className='range__sum'><span className='range__text'>Сумма для инвестирования</span> <div className='range__money'>
-         {Number((duration)*1000).toLocaleString('ru-RU')}&ensp;&#8381;</div></div>
-
-          <form className='range__form'>
-            <Slider
-              min={50}
-              max={3000}
-              marks={marks}
-              onChange={handleChangeValue}
-              step={1}
-              defaultValue={duration}
-              railStyle={{background:'grey',height: '10px'}}
-              trackStyle={{height: '10px'}}
-              handleStyle={{width: '25px',height: '25px'}}
-              dotStyle={{width: '15px',height: '15px', marginBottom: '-6px'}}
-            />
-   
-
-          </form>
-
-          </div>
-        
-    )
-  }
-
-export default Range
+export default Range;
